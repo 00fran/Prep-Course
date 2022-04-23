e0707bcc2fd837ccc4475a442d1165efbe195f6e
Francisco Nicolás Fernández <00franfernandez00@gmail.com>
2022-04-23 17:24:31 -0300

    a

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,14 +28,15 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  console.log(str)
-  
+  return str
+
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+  x+y
   
 }
 
